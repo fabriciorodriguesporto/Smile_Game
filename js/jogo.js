007384dfebe3ae9f9fe6aeb8132f4ed9cbb182ff
Fabricio rodrigues porto <fabriciorodriguesporto665@gmail.com>
2026-05-08 20:41:09 -0300

    Versão yugioh

--- js/jogo.js
+++ js/jogo.js
@@ -6,71 +6,113 @@ const btnReiniciar = document.getElementById("reiniciar");
 const btnJogarNovamente = document.getElementById("joganovamente");
 
 function verifica(obj) {
+
   if (!jogar) {
-    alert("Clique em Jogar novamente");
+    alert("Clique em Próximo Duelo");
     return;
   }
 
   jogar = false;
+
   tentativas++;
 
   let sorteado = Math.floor(Math.random() * 6);
 
   if (parseInt(obj.id) === sorteado) {
+
     acertou(obj);
+
     acertos++;
+
   } else {
+
     obj.classList.add("errou");
 
     let certo = document.getElementById(sorteado);
+
     acertou(certo);
   }
 
   atualizaPlacar();
 
   if (tentativas >= 3) {
+
     btnJogarNovamente.classList.add("invisivel");
+
     btnReiniciar.classList.remove("invisivel");
   }
 }
 
 function acertou(obj) {
+
   obj.classList.add("acertou");
 
-  const img = new Image(80);
-  img.src =
-    "https://upload.wikimedia.org/wikipedia/commons/2/2e/Oxygen480-emotes-face-smile-big.svg";
-
-  obj.appendChild(img);
+  obj.innerHTML = `
+  
+    <img 
+      src="./img/darkmagician.png"
+      style="
+        width: 140px;
+        height: 200px;
+        object-fit: cover;
+        border-radius: 8px;
+      "
+    >
+    
+  `;
 }
 
 function atualizaPlacar() {
-  let desempenho = tentativas === 0 ? 0 : (acertos / tentativas) * 100;
 
-  document.getElementById("resposta").innerHTML =
-    `Acertos: ${acertos} | Tentativas: ${tentativas} | Desempenho: ${Math.round(desempenho)}%`;
+  let desempenho =
+    tentativas === 0
+    ? 0
+    : (acertos / tentativas) * 100;
+
+  document.getElementById("resposta").innerHTML = `
+  
+    Vitórias: ${acertos}
+    |
+    Duelos: ${tentativas}
+    |
+    Desempenho: ${Math.round(desempenho)}%
+    
+  `;
 }
 
 function jogarNovamente() {
+
   jogar = true;
 
   document.querySelectorAll(".carta").forEach(c => {
+
     c.className = "carta";
-    c.innerHTML = `<span>${c.id}</span>`;
+
+    c.innerHTML = "<span>?</span>";
   });
 }
 
 function reiniciar() {
+
   tentativas = 0;
   acertos = 0;
   jogar = true;
 
   jogarNovamente();
+
   atualizaPlacar();
 
   btnJogarNovamente.classList.remove("invisivel");
+
   btnReiniciar.classList.add("invisivel");
 }
 
-btnJogarNovamente.addEventListener("click", jogarNovamente);
-btnReiniciar.addEventListener("click", reiniciar);
+btnJogarNovamente.addEventListener(
+  "click",
+  jogarNovamente
+);
+
+btnReiniciar.addEventListener(
+  "click",
+  reiniciar
+);
